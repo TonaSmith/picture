c4ba249b7f6be51f6bd4a0fc3a18a55f611a2287
TonaSmith <629363241@qq.com>
2021-11-18 20:44:54 +0800

    手动更新

--- randimgs.py
+++ randimgs.py
@@ -1,5 +1,5 @@
-filename="D:\\常用软件\\阿里云盘\\webp\\randimgs.txt"
-randimgs=open(filename,"n")
+filename="D:\\常用软件\\阿里云盘\\webp随机图\\randimgs.txt"
+randimgs=open(filename,"a")
 for numbers in range(1,169):
     randimgs.write("https://cdn.jsdelivr.net/gh/TonaSmith/picture/img/"+str(numbers)+".webp\n")
 randimgs.close()
